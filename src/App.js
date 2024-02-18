f07f6c183ca915af2c9572ad0bfbc769e9cd68e5
Szymon Brud <szymon.brud@crodu.com>
2024-02-18 11:31:04 +0100

    fix: update title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Hello } from './components/Hello/Hello';
 
 function App() {
-  return <Hello text={'HELLO NAVMAP cypress 2'} />;
+  return <Hello text={'HELLO NAVMAP'} />;
 }
 
 export default App;
